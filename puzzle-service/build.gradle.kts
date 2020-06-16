99a33e734bcad6f425a1a82380fc6222c88c58c6
nic0lasfara <nicolas.farabegoli@gmail.com>
2020-06-16 12:33:36 +0200

    Refactor library dependencies

--- puzzle-service/build.gradle.kts
+++ puzzle-service/build.gradle.kts
@@ -11,12 +11,19 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.vertx:vertx-core:3.9.1")
-    implementation("io.vertx:vertx-lang-kotlin:3.9.1")
-    implementation("io.vertx:vertx-web:3.9.1")
-    implementation("io.vertx:vertx-rabbitmq-client:3.9.1")
-    implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("org.slf4j:slf4j-simple:1.7.30")
+
+    with("io.vertx") {
+        implementation("$this:vertx-core:3.9.1")
+        implementation("$this:vertx-lang-kotlin:3.9.1")
+        implementation("$this:vertx-web:3.9.1")
+        implementation("$this:vertx-rabbitmq-client:3.9.1")
+        implementation("$this:vertx-lang-kotlin-coroutines:3.9.1")
+    }
+
+    with("org.slf4j") {
+        implementation("$this:slf4j-api:1.7.30")
+        implementation("$this:slf4j-simple:1.7.30")
+    }
 }
 
 tasks {
---
+++ puzzle-service/src/main/kotlin/it/unibo/pcd/puzzleservice/Routes.kt
@@ -0,0 +1,4 @@
+package it.unibo.pcd.puzzleservice
+
+object Routes {
+}
