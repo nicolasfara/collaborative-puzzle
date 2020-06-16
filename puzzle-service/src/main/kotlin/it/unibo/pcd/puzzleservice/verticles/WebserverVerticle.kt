98161e6ad7e62d00d0d94315336c6ffaee4c8981
nic0lasfara <nicolas.farabegoli@gmail.com>
2020-06-16 12:34:10 +0200

    Bind routes handle

--- puzzle-service/src/main/kotlin/it/unibo/pcd/puzzleservice/verticles/WebserverVerticle.kt
+++ puzzle-service/src/main/kotlin/it/unibo/pcd/puzzleservice/verticles/WebserverVerticle.kt
@@ -1,31 +1,45 @@
 package it.unibo.pcd.puzzleservice.verticles
 
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.Promise
-import io.vertx.core.json.JsonObject
+import io.vertx.core.http.HttpServer
+import io.vertx.ext.web.Router
+import io.vertx.kotlin.core.http.listenAwait
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.rabbitmq.startAwait
 import io.vertx.rabbitmq.RabbitMQClient
 import io.vertx.rabbitmq.RabbitMQOptions
+import it.unibo.pcd.puzzleservice.Routes
 import org.slf4j.LoggerFactory
 
-class WebserverVerticle : AbstractVerticle() {
+class WebserverVerticle : CoroutineVerticle() {
     private val logger = LoggerFactory.getLogger(WebserverVerticle::class.java)
     private lateinit var rabbitMQClient: RabbitMQClient
+    private lateinit var server: HttpServer
+    private lateinit var router: Router
+    private lateinit var routerManager: Routes
 
-    override fun start(startPromise: Promise<Void>?) {
+    override suspend fun start() {
         logger.info("${WebserverVerticle::class.java} started")
         val config = RabbitMQOptions()
         config.uri = "amqp://guest:guest@localhost"
         rabbitMQClient = RabbitMQClient.create(vertx, config)
-        rabbitMQClient.start {
-            if (it.succeeded()) {
-                logger.info("Connection to rabbitmq succeeded")
-            } else {
-                logger.error("Failed to connect to rabbitmq")
-            }
+        try {
+            rabbitMQClient.startAwait()
+        } catch (ex: Exception) {
+            logger.error("Error on connection", ex.cause)
         }
-    }
 
-    override fun stop(stopPromise: Promise<Void>?) {
-        logger.info("${WebserverVerticle::class.java} stopped")
+        router = Router.router(vertx)
+        routerManager = Routes(context)
+
+        router.get("/").handler(routerManager::entryPoint)
+        router.get("/api/create_puzzle").handler(routerManager::createPuzzle)
+        router.get("/api/join_puzzle").handler(routerManager::joinPuzzle)
+        router.get("/api/leave_puzzle").handler(routerManager::leavePuzzle)
+        router.get("/api/move").handler(routerManager::move)
+        router.get("/api/score").handler(routerManager::score)
+
+        vertx.createHttpServer()
+                .requestHandler(router)
+                .listenAwait(8080)
     }
 }
