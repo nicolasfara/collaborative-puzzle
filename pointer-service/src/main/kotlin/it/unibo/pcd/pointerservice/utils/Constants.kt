28f44886595ad9f3cb5b0afa367fbdc5d3dab90b
nic0lasfara <nicolas.farabegoli@gmail.com>
2020-06-26 10:20:48 +0200

    Implement pointer

--- pointer-service/src/main/kotlin/it/unibo/pcd/pointerservice/utils/Constants.kt
+++ pointer-service/src/main/kotlin/it/unibo/pcd/pointerservice/utils/Constants.kt
@@ -1,10 +1,8 @@
 package it.unibo.pcd.pointerservice.utils
 
 object Constants {
-    const val POINTER_QUEUE = "pointer.position"
-    const val POINTER_UPDATE_QUEUE = "pointer.update"
-    const val PUZZLE_WITH_USER_QUEUE = "puzzle.user"
-    const val PUZZLE_WITH_USER_RES_QUEUE = "puzzle.user.result"
+    const val EXCHANGE_NAME = "collaborative.puzzle"
+    const val POINTER_QUEUE = "pointer"
 
     const val NEW_POINTER_ADDRESS = "pointer.address"
 }
