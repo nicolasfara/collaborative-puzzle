e49daa1959796a00caca332a99be1889d4ecec2f
nic0lasfara <nicolas.farabegoli@gmail.com>
2020-06-26 11:46:15 +0200

    Add kotlin coroutine support

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     with("io.vertx") {
         implementation("$this:vertx-core:3.9.1")
+        implementation("$this:vertx-lang-kotlin:3.9.1")
+        implementation("$this:vertx-lang-kotlin-coroutines:3.9.1")
         implementation("$this:vertx-web-client:3.9.1")
     }
 }
---
+++ client/src/main/kotlin/it/unibo/pcd/client/verticles/NetManagerVerticle.kt
@@ -0,0 +1,6 @@
+package it.unibo.pcd.client.verticles
+
+import io.vertx.core.AbstractVerticle
+
+class NetManagerVerticle: AbstractVerticle() {
+}
